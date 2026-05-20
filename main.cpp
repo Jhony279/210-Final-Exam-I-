ada8dcc8a64a0b06011578632ca6c0b7cb458984
Jhony279 <lordjhony732@gmail.com>
2026-05-20 15:13:49 -0700

    Reading text file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 // COMSC-210 | Final 1 | Johnathan Perez Baltazar
 
 #include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
 using namespace std;
 
-// Function Prototypes:
+const string TXTFILE = "210-final-1-SP26.txt";
 
 /**
  * @brief - Description of main
  * @return 
 */
 int main() {
+    
+    ifstream file(TXTFILE);  // Open the file for reading
+    string line;
+    if (!file.is_open()) {  // Check if the file was opened successfully
+        cout << "Error opening file: " << TXTFILE << endl;
+        return 1;
+    }
+
     return 0;
 }
